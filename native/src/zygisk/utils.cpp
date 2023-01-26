d3bf722eabed57ed6f061e634ea9ee629ebe5485
Takeda-senpai <deadboltsx303@gmail.com>
2023-01-26 09:54:07 +0700

    Hide magisk module from maps

--- native/src/zygisk/utils.cpp
+++ native/src/zygisk/utils.cpp
@@ -1,5 +1,6 @@
 #include <cinttypes>
 #include <base.hpp>
+#include <sys/stat.h>
 
 #include "zygisk.hpp"
 #include <lsplt.hpp>
@@ -48,8 +49,7 @@ void fakemap_file(const char *name) {
     }
 }
 
-void remap_all(const char *name) {
-    auto maps = find_maps(name);
+static void remap_maps(vector<lsplt::MapInfo> maps) {
     for (auto &info : maps) {
         void *addr = reinterpret_cast<void *>(info.start);
         size_t size = info.end - info.start;
@@ -61,6 +61,26 @@ void remap_all(const char *name) {
         mremap(copy, size, size, MREMAP_MAYMOVE | MREMAP_FIXED, addr);
         mprotect(addr, size, info.perms);
     }
+}
+
+void remap_all(const char *name) {
+    auto maps = find_maps(name);
+    remap_maps(maps);
+}
+
+void hide_from_maps() {
+    auto maps = lsplt::MapInfo::Scan();
+    struct stat data_st;
+    if (stat("/data", &data_st))
+        return;
+    for (auto iter = maps.begin(); iter != maps.end();) {
+        if (!string(iter->path).starts_with("/") || string(iter->path).starts_with("/data/") || iter->dev != data_st.st_dev) {
+            iter = maps.erase(iter);
+        } else {
+            ++iter;
+        }
+    }
+    remap_maps(maps);
 }
 
 uintptr_t get_function_off(int pid, uintptr_t addr, char *lib) {
